57de206d49a29662e381fe254505883665f6b9b8
Ryan Krishandi Lukito <ryanlukito13@gmail.com>
2024-07-19 15:16:36 +0700

    add comment

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 
 dotenv.config();
-
+//
 const db = new Sequelize(
   process.env.POSTGRES_DATABASE,
   process.env.POSTGRES_USER,
